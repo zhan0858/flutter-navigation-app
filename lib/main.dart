b4e36f1c563d7dcc6e5784321148e965ac914c9b
zhan0858 <zhan0858@algonquinlive.com>
2024-11-25 13:31:27 -0500

    add BottomNavigationBar & Drawer and basic 3 screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
+import 'screens/contact_screen.dart';
+import 'screens/data_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,10 +13,84 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  final screens = [
+    const HomeScreen(),
+    const DataScreen(),
+    const ContactScreen()
+  ];
+  int currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Navigation App week13",
+          style: TextStyle(color: Colors.black),
         ),
+        backgroundColor: Colors.yellow,
+      ),
+      body: screens[currentIndex],
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            ListTile(
+              title: const Text("Home"),
+              onTap: () {
+                setState(() {
+                  currentIndex = 0;
+                });
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text("Data"),
+              onTap: () {
+                setState(() {
+                  currentIndex = 1;
+                });
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text("Contact"),
+              onTap: () {
+                setState(() {
+                  currentIndex = 2;
+                });
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        fixedColor: Colors.black,
+        backgroundColor: Colors.yellow,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.data_usage), label: "Data"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.contact_phone), label: "Contact"),
+        ],
+        onTap: (value) => {
+          setState(() {
+            currentIndex = value;
+          })
+        },
       ),
     );
   }
---
+++ lib/screens/data_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DataScreen extends StatelessWidget {
+  const DataScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text("I am a data screen");
+  }
+}
