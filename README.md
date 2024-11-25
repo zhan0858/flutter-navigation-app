8a43526562cb4bb8e1e00e2a56347853988fa39c
zhan0858 <zhan0858@algonquinlive.com>
2024-11-25 13:13:50 -0500

    init project

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_navigation_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flutter_navigation_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_navigation_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
